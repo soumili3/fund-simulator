006b36bbce132a45e800e7941b224be7278825b6
soumili3 <soumilipanja3@gmail.com>
2026-03-15 22:20:10 +0530

    temp rename footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import Footer from './components/footerTemp';
 import SimulatorPage from './pages/SimulatorPage';
 import HowItWorksPage from './pages/HowItWorksPage';
 import FundCategoriesPage from './pages/FundCategoriesPage';
